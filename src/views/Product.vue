a48ea8fe7def0d8f254ed6aed4496bbcc69c9957
lukman <nandes88.ni@gmail.com>
2020-10-03 01:18:39 +0700

    menambahkan product screen

---
+++ src/views/Product.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <HeaderSayna />
+    <p>product saya akan berada disini</p>
+    <FooterSayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderSayna from "@/components/HeaderSayna.vue";
+import FooterSayna from "@/components/FooterSayna";
+export default {
+  name: "product",
+  components: {
+    HeaderSayna,
+    FooterSayna,
+  },
+};
+</script>
