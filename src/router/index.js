7a610ba6ab38a08ee0d1e1407a409fb7d2dec534
lukman <nandes88.ni@gmail.com>
2020-10-03 03:30:48 +0700

    menambahkan halaman shoppingcart & success

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import Success from '../views/Success.vue'
 
 Vue.use(VueRouter)
 
@@ -22,6 +24,16 @@ const routes = [{
     name: 'product',
     component: Product
   },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: ShoppingCart
+  },
+  {
+    path: '/success',
+    name: 'success',
+    component: Success
+  }
 ]
 
 const router = new VueRouter({
